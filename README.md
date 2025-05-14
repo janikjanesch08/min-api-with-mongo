5e0de43728bdab179247e64c47b605f0d998038d
janikjanesch08 <janik.janesch@edu.gbssg.ch>
2025-05-14 15:38:13 +0200

    MongoDB-Anbindung mit Timeout und Fehlerbehandlung hinzugefügt

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # min-api-with-mongo
+
+docker run -d --name mongodb -p 27017:27017 -v mongodb-data:/data/db -e MONGO_INITDB_ROOT_USERNAME=gbs -e MONGO_INITDB_ROOT_PASSWORD=geheim mongo
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,37 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(configSection);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/check", (IOptions<DatabaseSettings> options) =>
+{
+    var connectionString = options.Value.ConnectionString;
+
+    try
+    {
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        var client = new MongoClient(connectionString);
+        var databases = client.ListDatabaseNames(cts.Token).ToList();
+        return Results.Ok("Zugriff auf MongoDB ok. Datenbanken: " + string.Join(", ", databases));
+    }
+    catch (TimeoutException ex)
+    {
+        return Results.Problem("Timeout beim Zugriff auf MongoDB: " + ex.Message);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem("Fehler beim Zugriff auf MongoDB: " + ex.Message);
+    }
+});
 
 app.Run();
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = "";
+}
